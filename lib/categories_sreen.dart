db7766bacb1c5ab9c07da15849dc6278df7d9326
kaibalya jena <kaibalya18j@gmail.com>
2022-08-09 22:15:18 +0530

    some

--- lib/categories_sreen.dart
+++ lib/categories_sreen.dart
@@ -6,12 +6,13 @@ class CategoriesSreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
+      padding: const EdgeInsets.all(15),
       children: [
         ...DUMMY_CATEGORIES.map((e) => CategoryItem(e.title, e.color)).toList()
       ],
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 1.5,
+          maxCrossAxisExtent: 300,
+          childAspectRatio: 1.15,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20),
     );
