893f19d3aa75719c13ffd0f3b3e600df6f969ef5
kaibalya jena <kaibalya18j@gmail.com>
2022-08-18 14:00:46 +0530

    ink well widget

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("meals in each category"),
+    );
+  }
+}
