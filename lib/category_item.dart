ce33e99a44b20eff6002c887069c2933255008cc
kaibalya jena <kaibalya18j@gmail.com>
2022-08-18 14:10:04 +0530

    navigator screen change like stack

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:makelicous/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   String title = "";
   Color color = Colors.orange;
   CategoryItem(this.title, this.color);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(
-              fontFamily: 'MouseMemoirs', fontSize: 28, color: Colors.black54),
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Colors.amberAccent,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+                fontFamily: 'MouseMemoirs',
+                fontSize: 28,
+                color: Colors.black54),
+          ),
         ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              color.withOpacity(0.3),
+              color.withOpacity(0.65),
+              color
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(20)),
       ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.3), color.withOpacity(0.65), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(20)),
     );
   }
 }
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,14 +1,17 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
-
+class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("meals in each category"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("dishes"),
+        backgroundColor: Color.fromARGB(255, 244, 131, 87),
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Text("meals in each category"),
+      ),
     );
   }
 }
